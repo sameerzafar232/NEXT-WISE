1ab6e548313962db29f51dac232f518641b36e70
sameerzafar232@gmail.com <sameerzafar232@gmail.com>
2024-01-06 07:17:18 -0800

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> */}
       <body className={inter.className}>{children}</body>
     </html>
   )
